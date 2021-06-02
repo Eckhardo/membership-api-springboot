46322c43bbc49ac154a8527128e49e289cb7553f
eckhardo <eckhard.kirschning@freenet.de>
2021-06-02 08:44:21 +0200

    Initialize Project

---
+++ src/main/java/com/eki/shipment/MembershipApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eki.shipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipApiSpringbootApplication.class, args);
+	}
+
+}
